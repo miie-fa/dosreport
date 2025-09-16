f2fe243c3438291125f48a0e0cf233b68f26a6a9
Farras <115802691+miie-fa@users.noreply.github.com>
2025-09-16 23:18:53 +0700

    Update report.py

--- report.py
+++ report.py
@@ -1,140 +1,21 @@
-Status    : 301 Moved Permanently
-Title     : 301 Moved Permanently
-IP        : 139.59.219.33
-Country   : AUSTRALIA, AU
-
-Summary   : HTTPServer[nginx], nginx, RedirectLocation[https://siak.polimedia.ac.id/]
-
-Detected Plugins:
-[ HTTPServer ]
-        HTTP server header string. This plugin also attempts to 
-        identify the operating system from the server header. 
-
-        String       : nginx (from server string)
-
-[ RedirectLocation ]
-        HTTP Server string location. used with http-status 301 and 
-        302 
-
-        String       : https://siak.polimedia.ac.id/ (from location)
-
-[ nginx ]
-        Nginx (Engine-X) is a free, open-source, high-performance 
-        HTTP server and reverse proxy, as well as an IMAP/POP3 
-        proxy server. 
-
-        Website     : http://nginx.net/
-
-HTTP Headers:
-        HTTP/1.1 301 Moved Permanently
-        Server: nginx
-        Date: Tue, 16 Sep 2025 15:58:39 GMT
-        Content-Type: text/html
-        Content-Length: 162
-        Connection: close
-        Location: https://siak.polimedia.ac.id/
-
-WhatWeb report for https://siak.polimedia.ac.id/
-Status    : 200 OK
-Title     : Sistem Informasi Akademik
-IP        : 139.59.219.33
-Country   : AUSTRALIA, AU
-
-Summary   : Bootstrap, Cookies[PHPSESSID], HTML5, HTTPServer[nginx], Meta-Author[pampersdry.info], Modernizr, nginx, PHP[7.2.34], Script[text/javascript], X-Powered-By[PHP/7.2.34], X-UA-Compatible[IE=edge], X-XSS-Protection[1; mode=block]
-
-Detected Plugins:
-[ Bootstrap ]
-        Bootstrap is an open source toolkit for developing with 
-        HTML, CSS, and JS. 
-
-        Website     : https://getbootstrap.com/
-
-[ Cookies ]
-        Display the names of cookies in the HTTP headers. The 
-        values are not returned to save on space. 
-
-        String       : PHPSESSID
-
-[ HTML5 ]
-        HTML version 5, detected by the doctype declaration 
-
-
-[ HTTPServer ]
-        HTTP server header string. This plugin also attempts to 
-        identify the operating system from the server header. 
-
-        String       : nginx (from server string)
-
-[ Meta-Author ]
-        This plugin retrieves the author name from the meta name 
-        tag - info: 
-        http://www.webmarketingnow.com/tips/meta-tags-uncovered.html
-        #author
-
-        String       : pampersdry.info
-
-[ Modernizr ]
-        Modernizr adds classes to the <html> element which allow 
-        you to target specific browser functionality in your 
-        stylesheet. You don't actually need to write any Javascript 
-        to use it. [JavaScript] 
-
-        Website     : http://www.modernizr.com/
-
-[ PHP ]
-        PHP is a widely-used general-purpose scripting language 
-        that is especially suited for Web development and can be 
-        embedded into HTML. This plugin identifies PHP errors, 
-        modules and versions and extracts the local file path and 
-        username if present. 
-
-        Version      : 7.2.34
-        Google Dorks: (2)
-        Website     : http://www.php.net/
-
-[ Script ]
-        This plugin detects instances of script HTML elements and 
-        returns the script language/type. 
-
-        String       : text/javascript
-
-[ X-Powered-By ]
-        X-Powered-By HTTP header 
-
-        String       : PHP/7.2.34 (from x-powered-by string)
-
-[ X-UA-Compatible ]
-        This plugin retrieves the X-UA-Compatible value from the 
-        HTTP header and meta http-equiv tag. - More Info: 
-        http://msdn.microsoft.com/en-us/library/cc817574.aspx 
-
-        String       : IE=edge
-
-[ X-XSS-Protection ]
-        This plugin retrieves the X-XSS-Protection value from the 
-        HTTP header. - More Info: 
-        http://msdn.microsoft.com/en-us/library/cc288472%28VS.85%29.
-        aspx
-
-        String       : 1; mode=block
-
-[ nginx ]
-        Nginx (Engine-X) is a free, open-source, high-performance 
-        HTTP server and reverse proxy, as well as an IMAP/POP3 
-        proxy server. 
-
-        Website     : http://nginx.net/
-
-HTTP Headers:
-        HTTP/1.1 200 OK
-        Server: nginx
-        Date: Tue, 16 Sep 2025 15:58:46 GMT
-        Content-Type: text/html; charset=UTF-8
-        Transfer-Encoding: chunked
-        Connection: close
-        X-Powered-By: PHP/7.2.34
-        Set-Cookie: PHPSESSID=e40n0uhs9pg2cnqt2bfo79n24b; path=/
-        Expires: Thu, 19 Nov 1981 08:52:00 GMT
-        Cache-Control: no-store, no-cache, must-revalidate
-        Pragma: no-cache
-        X-XSS-Protection: 1; mode=block
+└─$ nikto -h https://siak.polimedia.ac.id
+- Nikto v2.5.0
+---------------------------------------------------------------------------
++ Target IP:          139.59.219.33
++ Target Hostname:    siak.polimedia.ac.id
++ Target Port:        443
+---------------------------------------------------------------------------
++ SSL Info:        Subject:  /CN=siak.polimedia.ac.id
+                   Ciphers:  ECDHE-ECDSA-AES256-GCM-SHA384
+                   Issuer:   /C=US/O=Let's Encrypt/CN=E8
++ Start Time:         2025-09-16 23:12:29 (GMT7)
+---------------------------------------------------------------------------
++ Server: nginx
++ /: Cookie PHPSESSID created without the secure flag. See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies
++ /: Cookie PHPSESSID created without the httponly flag. See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies
++ /: Retrieved x-powered-by header: PHP/7.2.34.
++ /: The anti-clickjacking X-Frame-Options header is not present. See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
++ /: The site uses TLS and the Strict-Transport-Security HTTP header is not defined. See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
++ /: The X-Content-Type-Options header is not set. This could allow the user agent to render the content of the site in a different fashion to the MIME type. See: https://www.netsparker.com/web-vulnerability-scanner/vulnerabilities/missing-content-type-header/
++ No CGI Directories found (use '-C all' to force check all possible dirs)
++ /images: The web server may reveal its internal or real IP in the Location header via a request to with HTTP/1.0. The value is "10.130.223.0". See: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2000-0649
